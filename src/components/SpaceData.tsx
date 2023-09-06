1a67df0194bb41c60ef162fc1b99134bc3e191f4
kon-m <102782923+kon-m@users.noreply.github.com>
2023-09-06 18:05:31 +0900

    Update SpaceData.tsx

--- src/components/SpaceData.tsx
+++ src/components/SpaceData.tsx
@@ -5,7 +5,8 @@ const SpaceData: React.FC = () => {
   const [data, setData] = useState<any[]>([]);
 
   useEffect(() => {
-    fetch("https://mkon.pythonanywhere.com/space21", { mode: 'no-cors' })
+//    fetch("https://mkon.pythonanywhere.com/space21", { mode: 'no-cors' })
+      fetch("https://mkon.pythonanywhere.com/space21")
       .then((response) => response.json())
       .then((data) => setData(data))
       .catch((error) => console.error("Error fetching data:", error));
